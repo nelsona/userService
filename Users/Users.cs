f6452952def3d028deab9698a532dae77ecd3754
Alistair Nelson <alistair@alt-nelson.com>
2017-10-22 20:45:49 +0100

    Initial commit
    Rough structure in place. Includes main project and test project.

---
+++ Users/Users.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Users
+{
+    public class Class1
+    {
+    }
+}
---
+++ UsersTests/ValidationTests.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+namespace UsersTests
+{
+    [TestFixture]
+    public class ValidationTests
+    {
+        
+    }
+}
